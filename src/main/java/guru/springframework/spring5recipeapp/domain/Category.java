c9910e0bee59f54ee936815437993b517d86677c
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-06 17:50:37 +0100

    Add ManyToMany relationship

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring5recipeapp.domain;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descriprion;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
