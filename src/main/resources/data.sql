b2ce6ffb0ffb01bdc1a8342d8f85be8a2d22537f
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-06 22:25:13 +0100

    Add category and unitofmeasre data in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (descriprion) VALUES ('American');
+INSERT INTO category (descriprion) VALUES ('Italian');
+INSERT INTO category (descriprion) VALUES ('Mexican');
+INSERT INTO category (descriprion) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('TeaSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('TableSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Dunce');
