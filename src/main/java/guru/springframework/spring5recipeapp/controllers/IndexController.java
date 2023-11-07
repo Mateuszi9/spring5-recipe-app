58955b1566b2850e11d12afd9c0e33df45e3c1be
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-07 09:15:40 +0100

    Add bootstrap class with two recipe and list of recipes in thymeleaf.

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -4,30 +4,27 @@ import guru.springframework.spring5recipeapp.domain.Category;
 import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
 import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import guru.springframework.spring5recipeapp.services.RecipeServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
 @Controller
 public class IndexController {
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+
+    private final RecipeServices recipeServices;
+
+    public IndexController(RecipeServices recipeServices) {
+        this.recipeServices = recipeServices;
     }
 
     @RequestMapping({"", "/", "index"})
-    public String getIndexPage()
+    public String getIndexPage(Model model)
     {
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("TeaSpoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeServices.getRecipes());
 
         return "index";
     }
